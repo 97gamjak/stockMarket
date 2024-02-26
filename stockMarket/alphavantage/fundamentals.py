c606988a849944d83fb96350057197cb3841af0c
Jakob Gamper <97gamjak@gmail.com>
2024-02-26 23:56:37 +0100

    started implementing data retriever for alphavantage

---
+++ stockMarket/alphavantage/fundamentals.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class Fundamentals:
+    def __init__(self, api_key, ticker):
+        self.api_key = api_key
+        self.ticker = ticker
+
+    def get_income_statement(self):
+        pass
+
+    def get_balance_sheet(self):
+        pass
+
+    def get_cash_flow(self):
+        pass
